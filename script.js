317a13f0cf83e11f20e7622e2ad5de2322e3dd18
Md Mahmudunnobe <mahmud.nobe@edu.minerva.edu>
2022-11-27 20:04:33 -0500

    make the first carousel active and decrease the interval time

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ window.addEventListener('resize', function() {
     //console.log(window.innerWidth);
     carousel = d3.select('.carousel');
     if (window.innerWidth < 575) {
-        carousel.attr("data-mdb-interval", 16000)
+        carousel.attr("data-mdb-interval", 10000)
     }
     else {
-        carousel.attr("data-mdb-interval", 8000)
+        carousel.attr("data-mdb-interval", 5000)
     }
     //console.log(carousel.attr('data-mdb-interval'));
 })
